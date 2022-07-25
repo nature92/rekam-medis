4b5811a7881d51938cc3da0c9eef0450b2fae693
yusuf <yusuf@pindad.com>
2022-07-25 08:07:57 +0700

    update base_url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-// $config['base_url'] = 'http://it25.pindad.id/rekam-medis/';
+$config['base_url'] = 'https://klinikrahardja.id/';
 
 /*
 |--------------------------------------------------------------------------
