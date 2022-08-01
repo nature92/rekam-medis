6cf458498c0e62da1552c9febc9f37e2bf78ed1d
Yusuf <ahmad.yusuf49@gmail.com>
2022-08-01 11:19:45 +0700

    update githook

--- githook.php
+++ githook.php
@@ -14,7 +14,8 @@
 		'git reset --hard HEAD',
 		'git pull --no-edit',
 		'git status',
-//		'git submodule sync',
+		'date'
+		'lsb_release -a',
 //		'git submodule update',
 //		'git submodule status',
 	);
